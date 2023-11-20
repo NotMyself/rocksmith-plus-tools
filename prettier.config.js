fd0d5d5dd7b6c345c1c07d7f6680e34eb6fd4612
Bobby Johnson <bobby@notmyself.io>
2023-11-20 11:36:34 -0800

    cleanup

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  quoteProps: 'consistent',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
